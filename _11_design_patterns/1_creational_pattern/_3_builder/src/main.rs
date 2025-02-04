db982a0587cf2938814c806491470a227e7e9540
Abhi <abhilashmendhe@gmail.com>
2025-02-04 21:22:33 +0530

    worked on builder design pattern

--- _11_design_patterns/1_creational_pattern/_3_builder/src/main.rs
+++ _11_design_patterns/1_creational_pattern/_3_builder/src/main.rs
@@ -1,3 +1,56 @@
+#[derive(Debug)]
+struct Burger {
+    size: String, 
+    egg: bool,
+    extra_cheese: bool,
+    mayo: bool,
+    onion: bool,
+    lettuce: bool
+}
+
+impl Burger {
+
+    fn new() -> Self {
+        Burger {
+            size: "small".to_string(),
+            egg: false,
+            extra_cheese: false,
+            mayo: false,
+            onion: false,
+            lettuce: false
+        }
+
+    }
+    fn size(mut self, size: String) -> Self {
+        self.size = size;
+        self
+    }
+    fn egg(mut self, egg: bool) -> Self {
+        self.egg = egg;
+        self
+    }
+    fn extra_cheese(mut self, extra_cheese: bool) -> Self {
+        self.extra_cheese = extra_cheese;
+        self
+    }
+    fn mayo(mut self, mayo: bool) -> Self {
+        self.mayo = mayo;
+        self
+    }
+    fn onion(mut self, onion: bool) -> Self {
+        self.onion = onion;
+        self
+    }
+    fn lettuce(mut self, lettuce: bool) -> Self {
+        self.lettuce = lettuce;
+        self
+    }
+    fn build(self) -> Self {
+        self
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let burger = Burger::new().size("medium".to_string()).build();
+    println!("{:#?}",burger);
+    
 }
