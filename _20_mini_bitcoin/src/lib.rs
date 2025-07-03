084e5387e1579fe0f0434e48005848c8c3484d23
abhilashmendhe <abhilashmendhe@gmail.com>
2025-07-03 15:30:19 +0530

    minute modifiction in main.rs lib.rs files

--- _20_mini_bitcoin/src/lib.rs
+++ _20_mini_bitcoin/src/lib.rs
@@ -1,17 +1,1 @@
 pub mod finite_field;
-
-
-#[cfg(test)]
-mod tests {
-    use crate::finite_field::FieldElement;
-
-
-    #[test]
-    fn compare_finite_field() {
-        let a = FieldElement::new(7, 13);
-        let b = FieldElement::new(6, 13);
-        
-        assert!(a != b); 
-        // assert!(a == b); // Fails
-    }
-}
