42df1fc7d276ab0545b1ca1597f7ef79561f9807
Abhilash <abhilashmendhe@gmail.com>
2026-06-03 21:45:50 +0530

    restructured search. added another method to insert key in skip-list

--- _22_skip_list/src/skip_list.rs
+++ _22_skip_list/src/skip_list.rs
@@ -24,12 +24,11 @@ impl SkipList {
         levels
     }
 
-    pub fn search(&self, target: i64) -> bool {
+    fn in_search(&self, target: i64) -> (NonNull<Node>, Vec<Option<NonNull<Node>>>) {
+        let mut current = self.head;
+        let mut update = vec![None; self.levels + 1];
         unsafe {
-            let mut current = self.head;
-            let mut i = self.levels;
-
-            while i > 0 {
+            for i in (0..=self.levels).rev() {
                 while let Some(nnode) = (&(*current.as_ptr()).forward)[i] {
                     if (*nnode.as_ptr()).key < target {
                         current = nnode;
@@ -37,15 +36,59 @@ impl SkipList {
                         break;
                     }
                 }
-                i -= 1;
+                update[i] = Some(current);
             }
-            if let Some(nnode) = (&(*current.as_ptr()).forward)[0] { 
-                if (*nnode.as_ptr()).key == target {
-                    return true;
+            if let Some(nnode) = (&(*current.as_ptr()).forward)[0] {
+                current = nnode;
+            }
+        }
+        (current, update)
+    }
+
+    pub fn search(&self, target: i64) -> bool {
+        let (current, _) = self.in_search(target);
+        unsafe {
+            if (*current.as_ref()).key == target {
+                return true;
+            } else {
+                false
+            }
+        }
+    }
+
+    pub fn insert(&self, key: i64) {
+        let (_current, update) = self.in_search(key);
+        let new_node_level = self.random_levels();
+        unsafe {
+            let new_node_tower =
+                NonNull::new_unchecked(Box::into_raw(Box::new(Node::new(key, new_node_level))));
+            for i in 0..=new_node_level {
+                let pred = update[i];
+                // let succ = (&(*update[i].unwrap().as_ptr()).forward)[i];
+                if let Some(succ) = update[i] {
+                    (&mut (*new_node_tower.as_ptr()).forward)[i] = (&(*succ.as_ptr()).forward)[i];
+                }
+                if let Some(predd) = pred {
+                    (&mut (*predd.as_ptr()).forward)[i] = Some(new_node_tower);
                 }
             }
         }
-        false
+    }
+
+    pub fn display(&self) {
+        println!("\n*******Skip List********");
+        let head = self.head;
+        for level in 0..=self.levels {
+            print!("Level {}: ", level);
+            unsafe {
+                let mut node = (&(*head.as_ptr()).forward)[level];
+                while let Some(n) = node {
+                    print!("{} ", (*n.as_ptr()).key);
+                    node = (&(*n.as_ptr()).forward)[level];
+                }
+            }
+            println!();
+        }
     }
 }
 
