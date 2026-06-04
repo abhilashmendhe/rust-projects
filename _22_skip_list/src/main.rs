409f32b170d91f6bc25b7947b69aee756a63ba84
Abhilash <abhilashmendhe@gmail.com>
2026-06-04 09:34:25 +0530

    some testing in main.rs

--- _22_skip_list/src/main.rs
+++ _22_skip_list/src/main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use std::ptr::NonNull;
+
+use _22_skip_list::skip_list::SkipList;
+
+fn main() {
+    // let mut value = 123;
+    // unsafe {
+    //     let x = NonNull::new(&mut value);
+    //     println!("{}", x.unwrap().as_ref());
+    //     if let Some(v) = x {
+    //         (*v.as_ptr()) = 10;
+    //     }
+    //     println!("{}", *x.unwrap().as_ptr());
+    // }
+    // println!("{}", value);
+
+    let skip_list = SkipList::new(0.5, 3);
+    skip_list.insert(1);
+    skip_list.insert(2);
+    skip_list.insert(4);
+    skip_list.insert(3);
+    skip_list.display();
+
+    skip_list.erase(0);
+    skip_list.display();
+
+    skip_list.erase(2);
+    skip_list.display();
+}
